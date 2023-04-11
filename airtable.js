3c1037be50296784f1ab2c328fbc0cbe4799a658
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-11 07:59:31 -0700

    updated airtable key with process variable

--- airtable.js
+++ airtable.js
@@ -1,8 +1,8 @@
 // airtable.js
 import nextConnect from 'next-connect';
 
-const apiKey = 'your_api_key';
-const baseId = 'base_id_goes_here';
+const apiKey = process.env.AIRTABLE_API_KEY;
+const baseId = 'appB6PVySBlUkfh3M';
 const tableName = encodeURIComponent('Topics');
 const fields = encodeURIComponent('fields[]=Topic&fields[]=Title&fields[]=Content');
 const url = `https://api.airtable.com/v0/${baseId}/${tableName}?${fields}`;
