f6848fae8ca09cf37130810fcee43d967aa8d5a6
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-12 08:09:02 -0700

    added error handling

--- pages/api/airtable.js
+++ pages/api/airtable.js
@@ -9,16 +9,28 @@ const url = `https://api.airtable.com/v0/${baseId}/${tableName}?${fields}`;
 
 const handler = nextConnect()
   .get(async (req, res) => {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${apiKey}`,
-        'Content-Type': 'application/json'
-      }
-    });
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${apiKey}`,
+          'Content-Type': 'application/json',
+        },
+      });
 
-    const data = await response.json();
-    res.status(200).json({ records: data.records });
+      if (!response.ok) {
+        console.error('Airtable API response error:', response.statusText);
+        return res.status(response.status).json({ error: response.statusText });
+      }
+
+      const data = await response.json();
+      const records = data.records;
+
+      res.status(200).json(records);
+    } catch (error) {
+      console.error('Error fetching data from Airtable:', error);
+      res.status(500).json({ error: 'Error fetching data from Airtable' });
+    }
   });
 
 export default handler;
