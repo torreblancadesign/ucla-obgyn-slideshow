9207c1d40f7d59fca32d5cf52fbe618cafb276f7
Enrique Torreblanca <enriquetchat@gmail.com>
2023-05-01 18:59:38 -0700

    updated the name of the content field in AT

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ const IndexPage = () => {
           <h2 className="slide-title">{slide.fields.Topic}</h2>
           <div
             className="slide-content"
-            dangerouslySetInnerHTML={{ __html: slide.fields.Content }}
+            dangerouslySetInnerHTML={{ __html: slide.fields.Content_html }}
           ></div>
         </div>
       ))}
